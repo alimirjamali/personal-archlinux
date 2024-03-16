05db090db6e746409e651b717cc47e8343d33e64
Ali Mirjamali <ali.mirjamali@gmail.com>
2024-03-16 17:25:40 +0330

    Clean Sweep

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
-# About this repository
+# Tools, Configs, Customization & Tweaks for Archlinux
+This repository is from my early days of Archlinux. While I was getting used to
+this Distro.
+
 ## My personal installation scripts for Arch Linux
-
-While experimenting with Arch Linux, I have decided to write a simple bash script to keep my life much easier.
-
-## KICKS
-Keep It a Complete Killjoy Script.
+A simple bash script with the list of packages to be installed during the
+initial pacstap transaction. This is really old and obsolete. It is necessary to
+switch to a modern cross-platform approach.
 
 ## Themes
 #### GRUB
-I am currently using a [modified Vimix](https://github.com/vinceliuice/grub2-themes/tree/master/grub-theme-vimix/Vimix/icons) theme for 
+I was using a 
+[modified Vimix](https://github.com/vinceliuice/grub2-themes/tree/master/grub-theme-vimix/Vimix/icons) 
+theme for 
 [GRUB](https://wiki.archlinux.org/index.php/GRUB/Tips_and_tricks#Theme).
-I should fork the mentioned theme to add custom ICONs for shutdown, reboot and firmware. A custom GRUB menu file is added to repo.
-#### Plymouth
-[Plymouth](https://wiki.archlinux.org/index.php/plymouth) is installed via AUR. I am using the solar theme.
-#### SDDM
-I am using breeze theme for SDDM.
+I planned to fork the mentioned theme and add custom ICONs for shutdown, reboot 
+and reboot to firmware. A custom GRUB menu file is added to repo.
 
-# License
-Isn't it obvious to you that this should be GPLv2.0 ???
+#### Plymouth
+[Plymouth](https://wiki.archlinux.org/index.php/plymouth) is installed via AUR.
+I was using the solar theme.
+
+#### SDDM
+I was using breeze theme for SDDM.
--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "KICKS == Keep It a Complex Killjoy Script"
 echo "Installing Arch Linux to /mnt/arch ..."
 echo
 
 ### Updating Keyring, installation media is too old!!!
 # echo pacman -Sy --noconfirm archlinux-keyring
 
-# Let's assume that the drive is partitioned and ...
-# Everything should be mounted on the directory provided as argument or /mnt/arch
+# Let's assume that the drive is partitioned and all are mounted on the 
+# directory provided as argument or /mnt/arch
+
 if [ -z "$1" ]
 	then
 		install_target="/mnt/arch"
@@ -17,125 +17,130 @@ if [ -z "$1" ]
 fi
 if [ ! -d $install_target ]
 	then
-		echo "Nor any installation directory was provided as an argument, neither the default /mnt/arch exists. Exiting!"
-		exit
+		echo "Target installation directory does not exist!. Either "\
+			"mount target partitions on /mnt/arch or provide the "\
+			"target installation directory as an argument."
+		exit 1
 fi
 
-# swap should be enabled (if available).
+echo "Swap should be enabled (if available)."
 
 ### Selecting packages for installation
 declare -a pkgs=()
 # base packages
-pkgs+=(base)					# Arch base packages
+pkgs+=(base)				# Arch base packages
 
 ### Other desired packages
-pkgs+=(linux-headers dkms) 		# Linux Headers for Dynamic Kernel Module Support (if needed)
+pkgs+=(linux-headers dkms) 		# Linux Headers for dkms (if needed)
 pkgs+=(linux linux-hardened linux-lts) 	# Kernel & alternative Kernels if needed
 pkgs+=(linux-firmware)			# Kernel Firmware of devices
 pkgs+=(mkinitcpio dracut booster)	# initramfs generators
 pkgs+=(man-db)				# Offline manuals
-pkgs+=(sudo opendoas)			# Who keeps root account enabled these days???
-pkgs+=(pacman-contrib) 			# Pokémon add-ons
-pkgs+=(btrfs-progs)			# I am a btrfs user :-/
+pkgs+=(sudo opendoas)			# Root account should be disabled later
+pkgs+=(pacman-contrib) 			# Pacman add-ons
+pkgs+=(btrfs-progs)			# I am a loyal BTRFS user
 pkgs+=(compsize)			# ... with online zlib/zstd compression
+# pkgs+=(grub-btrfs) 			# btrfs snapshots for grub
+# pkgs+=(snapper snap-sync snap-pac)	# openSUSE snapper tools
 pkgs+=(ntfs-3g)				# Better NTFS support
 pkgs+=(exfat-utils)			# ExFat filesystem support
 pkgs+=(f2fs-tools)			# F2FS fielsystem support
 pkgs+=(sshfs)				# SFTP based mounts
-#! pkgs+=(amd-ucode intel-ucode)	# Should I install the damned close source Microcodes???
-pkgs+=(grub grub-customizer)		# Obviously we need a boot loader (and we like to customize it).
-#! pkgs+=(grub-btrfs) 			# btrfs snapshots for grub
+# pkgs+=(amd-ucode intel-ucode)		# Closed source Microcodes if needed
+pkgs+=(grub grub-customizer)		# Boot loader and customizer
 pkgs+=(os-prober)			# Detecting other OSes for grub
-pkgs+=(syslinux) 			# syslinux to chainload HDT (Hardware Detection Tool)
+pkgs+=(syslinux) 			# syslinux to chain-load HDT (Hardware Detection Tool)
 pkgs+=(memtest86+)			# memtest86+ for grub
-pkgs+=(efibootmgr)			# We will need efibootmgr if booting via UEFI
-pkgs+=(htop)				# How much memory I am using?
-pkgs+=(iotop)				# Why the storage is so damn slow??
-pkgs+=(glances)				# Sexy tool to monitor system resources
+pkgs+=(efibootmgr)			# efibootmgr if booting via UEFI
+pkgs+=(htop)				# Monitoring resources and processes
+pkgs+=(iotop)				# Monitoring storage
+pkgs+=(glances)				# Nice tool to monitor system resources
 pkgs+=(dmidecode) 			# To see details about RAM modules
 pkgs+=(i2c-tools) 			# I2C tools to see FAN RPM
 pkgs+=(ncdu duf)			# How much disk space I am using?
 pkgs+=(mc)				# Nostalgia :-)
-pkgs+=(tmux screen)			# I go both ways (in a good way)
-pkgs+=(vim vim-plugins)			# How can we live without vim ???
-pkgs+=(screenfetch)			# Showing off
+pkgs+=(tmux screen)			# Screen multiplexers
+pkgs+=(vim vim-plugins)			# RIP Bram Moolenaar
+pkgs+=(screenfetch)			# Showing system config
 pkgs+=(lshw)				# Tools to list hardware
-pkgs+=(fwupd)				# To upgrade firmware (has anyone over used this? :-o)
-pkgs+=(lsof)				# No only list of open files, but also list of open ports.
-pkgs+=(unzip)				# This is not to unzip a partner ;-)
-pkgs+=(unrar) 				# People still use RAR :-/
-# pkgs+=(snapper snap-sync snap-pac)	# openSUSE snapper tools
+pkgs+=(fwupd)				# To upgrade firmware
+pkgs+=(lsof)				# List of open files and open ports
+pkgs+=(unzip)				# No comment
+pkgs+=(unrar) 				# Just in case
 pkgs+=(mlocate)				# Where the hell are my files???
 pkgs+=(zip unzip)			# But why not gzip?
-pkgs+=(cups cups-pdf)			# I hate printers and PDF files :-/
-pkgs+=(arch-install-scripts) 		# If I ever want to install to external media (or update fstab)
-pkgs+=(hdparm sdparm smartmontools)	# Utilities to manage spinning rust and NAND
+pkgs+=(p7zip)				# 7-Zip files
+pkgs+=(cups cups-pdf)			# Printers and PDF creation
+pkgs+=(arch-install-scripts) 		# Easy fstab update or installing on USB
+pkgs+=(hdparm sdparm smartmontools)	# To monitor spinning rust and NAND
 # Networking / Internet
 pkgs+=(networkmanager)			# We need a NetworkManager (do not forget to enable the service)
 pkgs+=(iptables-nft)			# nftables
-pkgs+=(traceroute) 			# Where am i going to?
-pkgs+=(openssh)				# We should be able to SSH/SFTP to this PC
-pkgs+=(iw wpa_supplicant dhclient)	# Who uses Ethernet these days ???
-pkgs+=(lynx links)			# Text based browsers just if needed
+pkgs+=(traceroute) 			# Tracing packets
+pkgs+=(openssh)				# Allowing SSH to this machine
+pkgs+=(iw wpa_supplicant dhclient)	# Wireless and DHCP
+pkgs+=(lynx links)			# Text based browsers if needed
 pkgs+=(aria2 curl wget)			# Download tools
-pkgs+=(mutt)				# Reading emails
+pkgs+=(mutt)				# Reading emails in terminal
 pkgs+=(iftop nethogs nload)		# Network usage monitoring tools
-pkgs+=(dnscrypt-proxy)			# We are anonymous!
-pkgs+=(tor nyx torsocks torbrowser-launcher)	# Really anonymous!!!
+pkgs+=(dnscrypt-proxy)			# Encrypting DNS
+pkgs+=(tor nyx torsocks torbrowser-launcher)	# Anonymous!!!
 pkgs+=(dnsutils)			# Whois who?
-pkgs+=(speedtest-cli)			# How fast we are going?
-pkgs+=(vnstat) 				# ... and how much data i have consumed !?
-pkgs+=(ethtool)				# I love wired connections :-D
+pkgs+=(speedtest-cli)			# Measuring Internet speed
+pkgs+=(vnstat) 				# Consumed traffic
+pkgs+=(ethtool)				# Ethernet tools
 pkgs+=(gnu-netcat)			# Listening to network packets
-pkgs+=(ntp)				# What time it is?
-pkgs+=(samba)				# Sharing files with poor Wandozee users
-#! pkgs+=(openvpn)			# Will be deleted in favor of Wireguard
+pkgs+=(ntp)				# Time keeping
+pkgs+=(samba)				# Sharing files with Windows users
+#! pkgs+=(openvpn)			# OpenVPN
 ### Development
 pkgs+=(code)				# VSCode
-pkgs+=(gcc clang llvm)			# Two competing FOSS compilers 
-pkgs+=(arduino)				# Are we doing embedded development?
-pkgs+=(kicad)				# ... or EDA?
-pkgs+=(git)				# Hello Linus
-pkgs+=(base-devel)			# Maybe some development
-pkgs+=(python)				# Everyone is a parseltongue these days
-pkgs+=(go go-tools gcc-go)		# Some packages need golang to compile (but why am I installing the GCC variant?)
-pkgs+=(rust)				# Cool kids code with rust these days
+#! pkgs+=(atom)				# The other Microsoft/Github code editor
+pkgs+=(gcc clang llvm)			# Main Compilers
+pkgs+=(arduino)				# The Arduino
+pkgs+=(kicad)				# EDA
+pkgs+=(git)				# Source version control
+pkgs+=(base-devel)			# Development tools
+pkgs+=(python)				# The latest Python
+pkgs+=(go go-tools gcc-go)		# Some packages need golang to compile
+pkgs+=(rust)				# Rust
 pkgs+=(qt5-base qt6-base qt5-doc qt6-doc)	# Some GUI development
 pkgs+=(emscripten) 			# WebAssembly
-#! pkgs+=(atom)				# But why not vim? Why? Why? Why?
 #! pkgs+=(fpc fpc-src lazarus qt5pas)	# Free Pascal for nostalgic people
 ### GUI
 # X Window
-pkgs+=(xorg xorg-apps)			# Some Graphics plz.
-pkgs+=(xorg-fonts xorg-drivers)		# Moar Graphiczzz.
-# I ain't no i3 user :-P
-#! pkgs+=(plasma kde-applications)		# KDE Plasma???
+pkgs+=(xorg xorg-apps)			# Should switch to Wayland
+pkgs+=(xorg-fonts xorg-drivers)		# Fonts and drivers
+#! pkgs+=(plasma kde-applications)	# KDE Plasma???
 #! pkgs+=(gnome gnome-extra)		# Gnome???
-pkgs+=(xfce4 xfce4-goodies)		# This is my prefered Desktop Environment
+pkgs+=(xfce4 xfce4-goodies)		# My preferred Desktop Environment
 pkgs+=(lightdm lightdm-gtk-greeter)	# Display manager for XFCE (and others)
 # GUI applications from here #
 pkgs+=(gnu-free-fonts noto-fonts)	# Some fonts
-pkgs+=(pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber helvum)	# Sound system
-pkgs+=(audacity)			# I can talk
+pkgs+=(pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber helvum)
+pkgs+=(audacity)			# Working with Audio
 pkgs+=(vlc)				# We need the traffic cone :-)
-pkgs+=(obs-studio)			# Screen recodering
-pkgs+=(kdenlive)			# NLE for dummies
+pkgs+=(obs-studio)			# Screen recording and streaming
+pkgs+=(kdenlive)			# NLE
 pkgs+=(openshot)			# Isn't Kdenlive good enough?
 pkgs+=(handbrake handbrake-cli)		# To transcode screen recordings
-pkgs+=(firefox)				# We need firefox browser
-pkgs+=(chromium)			# ... and chromium!!! 
-pkgs+=(libreoffice-fresh)		# Tools used once a week
-pkgs+=(krita)				# To draw manga
-pkgs+=(gimp)				# Does anyone use this?
-pkgs+=(inkscape)			# Vector
-pkgs+=(keepassxc)			# Who needs online password managers?
-pkgs+=(workrave redshift)		# To protect my health (also consider plasma5-applets-redshift-control if using KDE Plasma)
-pkgs+=(gsmartcontrol)			# Getting S.M.A.R.Ter
+pkgs+=(firefox)				# We need the Firefox browser
+pkgs+=(chromium)			# ... and chromium
+pkgs+=(libreoffice-fresh)		# Spreadsheets, Docs and Presentations
+pkgs+=(krita)				# Easy to use raster graphics editor
+pkgs+=(gimp)				# Not so easy to use graphics editor
+pkgs+=(inkscape)			# Vector graphics
+pkgs+=(keepassxc)			# My preferred password manager
+pkgs+=(workrave redshift)		# To protect my health. Also consider
+					# plasma5-applets-redshift-control
+					# if using KDE Plasma)
+pkgs+=(gsmartcontrol)			# Monitoring S.M.A.R.T status via GUI
 # Virtualization tools
-pkgs+=(libvirt virt-manager bridge-utils) 	# In case we need VMs (enable libvirtd)
 pkgs+=(qemu-full)			# In case we require to emulate other archituctures
-pkgs+=(dosbox)				# I only play old school games :-P
-pkgs+=(yt-dlp)	 			# Downloading Youtube videos for offline access :-P
+pkgs+=(libvirt virt-manager) 		# In case we need VMs (enable libvirtd)
+pkgs+=(bridge-utils)			# To bridge VMs
+pkgs+=(dosbox)				# To play old school games
+pkgs+=(yt-dlp)	 			# Downloading Youtube videos
 pkgs+=(gparted)				# Partitioning for dummies
 pkgs+=(util-linux parted gptfdisk)	# ncurses partitioning
 
